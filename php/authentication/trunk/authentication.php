4e7758333f53ffcd0c89d77a00c1b6373a9760cd
cyrus-mc <cyrus.drive@gmail.com>
2007-12-05 23:47:03 +0000

    Updated code to conform with PHP 5.0

--- php/authentication/trunk/authentication.php
+++ php/authentication/trunk/authentication.php
@@ -10,98 +10,8 @@
 	$Revision: $
 */
 
-define(__DEFAULTPASSWORDMODE__, "ENCRYPTED");
-
 function __autoload($class_name) {
 	   require_once 'dbase/' . $class_name . '.php';
-}
-
-// class used to store information about currently logged in user
-class UserBean {
-  	private $uname, $uid, $pmode = __DEFAULTPASSWORDMODE__;
-	private $udata = array();
-	private $database;
-
-	public function setDB($db) {
-		$this->database = $db;
-	}
-   /*
-      Default constructor.
-
-		Set username and userID
-   */
-	function __construct($uname, $uid) {
-		$this->uname = $uname;
-		$this->uid = $uid;
-	}
-
-	public function getUserName() { return $this->uname; } 
-
-	public function getDatabase() { return $this->database; }
-
-   /*
-      Retrieve field from this array if it exists, else returns FALSE 
-   */
-	public function getValue($key) {
-		if (isset($this->udata[$key])) {
-			return $this->udata[$key];
-		} else {
-			return FALSE;
-		}
-	}
-
-   /*
-      Set the value associated with supplied key. If key doesn't exist
-      it will be added.
-   */
-	public function setValue($key, $value) {
-		$this->udata[$key] = $value;
-	}
-
-	/*
-		Remove key and associated value from this.  If key doesn't exist
-		return FALSE, else TRUE 
-   */
-	public function removeValue($key) {
-		if (isset($this->udata[$key])) {
-			unset($this->udata[$key]);
-			return TRUE;
-		} else {
-			return FALSE;
-		}
-	}
-
-	/* 
-		Set the password mode (either ENCRYPTED or UNENCRYPTED)
-
-		Parameter: mode
-		values: e = ENCRYPTED, p = UNENCRYPTED
-	*/
-	public function setPasswordMode($mode) {
-		if ($mode == "e") {
-			$this->pmode = "ENCRYPTED";
-		} elseif ($mode == "p") {
-			$this->pmode = "UNENCRYPTED";
-		} else {
-			die("Invalid password mode : " . $mode);
-		}
-		return $this->pmode;
-	}
-	
-	/*
-		Set the user password (this is the database user password)
-	*/
-	public function setPassword($npasswd) {
-		$sql = "ALTER USER " . $this->uname . " ENCRYPTED PASSWORD '" . $npasswd . "'";
-	}
-
-	/*
-      Print the current values associated with the UserBean
-   */
-   public function printThis() {
-		print_r($this);
-		print_r($this->udata);
-   }
 }
 
 // default permission level and redirect page
@@ -109,64 +19,95 @@ define(__REDIRECTPAGE__, "/access_denied.html");
 
 /* 
 	Class used to authenticate user on a page per page basis.
-	pcontext = context of the page. This is compared with the uname to determine
-		        permission
+
+	** note: singleton class - can not be instantiated with new
 */
-class Authenticate {
-   var $pcontext, $epage, $authorized = 1;
-	var $ubean;
+class Authentication {
+
+	private $permissions = array(), $ref_count;
+	private static $instance = null;
+
+	private static $object_string = "authentication_object";
 
 	/*
-		Default constructor.  Sets page permission level and user permission
-		level to this.
+		Default constructor
+
+		** note: private so that class can not be instantiated by new
 	*/
-   function __construct($pcontext, $epage = __REDIRECTPAGE__) {
-		if (strlen($pcontext) != 0) {
-			$this->pcontext = substr($pcontext, 2);
-		} else
-			$this->pcontext = $pcontext;
+	private function __construct($permissions) {
+		$this->ref_count = 0;
 
-		$this->epage = $epage;
-
-		/* re-construct userBean, if it doesn't exist user has not logged in */
-		$this->ubean = unserialize($_SESSION["ubean"]);
-		if (!$this->ubean) {
-			header("Location: " . $epage);
-			session_destroy();
-		}
-      $this->checkAuth();
-   }
-
-	/*
-		Check to see if user permissions are good enough to access this page
-		(pcontext = uname)
-	*/
-   public function checkAuth() {
-		if (strlen($this->pcontext) != 0 && $this->pcontext != $this->ubean->getUserName()) {
-			$this->authorized = 0;
-			header("Location: " . $this->epage);
-		}
-   }
-
-	/*
-		Retrieve the userBean associated with this authentication request
-	*/
-	public function getUserBean() {
-		return $this->ubean;
+		/* set the permissions */
+		$this->set($permissions, TRUE);
 	}
 
-	/*
-		Set the page context for this authentication request
+	/* 
+		The singleton method
+				      
+		Returns only one instance of the class
 	*/
-	public function setPageContext($context) {
-		$this->pcontext = $context;
+	public static function getInstance($permissions = null) {
+
+		if (!isset(self::$instance)) {
+			$c = __CLASS__;
+			self::$instance = new $c($permissions);
+		}
+		/* increment reference count */
+		self::$instance->ref_count++;
+
+		return self::$instance;
 	}
 
-	/*
-		Return the authentication flags (1 = auth, 0 = denied)
+	/* 
+		Release object. Clear memory if ref_count = 0
 	*/
-   public function getAuthorized() {
-      return $this->authorized;
-   }
+	public function release($clear = FALSE) {
+		/* decrement ref_count */
+		$this->ref_count--;
+
+		if ($this->ref_count == 0 || $clear_mem) {
+			echo "Clearing permissions array\n";
+			$this->clear();
+		}
+	}
+
+	/* 
+		Clear the permissions array
+	*/
+	public function clear() {
+		foreach (array("application", "content", "user_id", "group_id") as $label) {
+			unset($this->permissions[$label]);
+			$this->permissions[$label] = array();
+		}
+	}
+
+	/* 
+		Set the permissions array
+	*/
+	public function set($permissions, $clear = FALSE) {
+		/* clear first if flag is set */
+		if ($clear)
+			$this->clear();
+
+		/* loop over rows of supplied argument and add to appropriate array */
+		foreach ($permissions as $row) {
+			$this->permissions["application"][] = current($row);
+			$this->permissions["content"][] = next($row);
+			$this->permissions["user_id"][] = next($row);
+			$this->permissions["group_id"][] = next($row);
+		}
+	}
+
+	/* 
+		Check access
+	*/
+	public check($application, $content, $user_id, $group_id) {
+	}
+
+	public function get() {
+		return $this->ref_count;
+	}
+
+	public function __toString() { return self::$object_string; }
 }
 ?>
