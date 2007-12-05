bb79de40c8ee9b9a8049bd9fadb77ed132b58b37
cyrus-mc <cyrus.drive@gmail.com>
2007-12-05 23:46:34 +0000

    Updated code to conform with PHP 5.0

--- php/common/trunk/functions.php
+++ php/common/trunk/functions.php
@@ -2,25 +2,24 @@
 /*
    vim:ts=3:sw=3:
 
-   Common php functions are defined and implemented in this file
+   Common PHP functionality implemented in this file
 
    $Author: cyrus $
    $Date: 2004/01/23 23:37:46 $
    $Revision: 1.2 $
 */
 
-// variable used to point to form response type
 $response = "";
+/* set the response variable so we can access post/get fields through a
+   common interface */
+if ( count($HTTP_POST_VARS) > 0 )
+	$response = $HTTP_POST_VARS;
+else 
+	$response = $HTTP_GET_VARS;
 
-/*
-	Function use to set response variable
-*/
-function setResponse() {
-	global $HTTP_POST_VARS, $HTTP_GET_VARS, $response;
-
-	if ( count($HTTP_POST_VARS) > 0 )
-		$response = $HTTP_POST_VARS;
-	else 
-		$response = $HTTP_GET_VARS;
+/* set base URL for requests - takes into consideration user directories */
+$baseURL = "";
+if ($_SERVER['REQUEST_URI'][1] == "~") {
+	$baseURL = "/" . substr($_SERVER['REQUEST_URI'], 1, strpos($_SERVER['REQUEST_URI'], '/', 1) - 1);
 }
 ?>
